e1a384cc2ae4b8580bd3aafead27d881bfd4154a
danielthecode <35077377+danielthecode@users.noreply.github.com>
2022-04-26 07:06:05 +0100

    Service to add, update, retrive and delete images from firebase firestore and storage

---
+++ sinkiwe/src/utils/ImageService.js
@@ -0,0 +1,61 @@
+
+import { storage } from '../firebase'
+import { getDocs, onSnapshot, addDoc, updateDoc, getDoc, deleteDoc, doc, serverTimestamp, query, where, orderBy } from "firebase/firestore"
+import {
+    deleteObject,
+    ref
+  } from "firebase/storage";
+
+
+class ImageService {
+
+    addImage = (collection, newPortraiture) => {
+        return addDoc(collection, {
+            imageURL: newPortraiture,
+            timeStamp: serverTimestamp()
+        })
+    }
+
+    updateImage = (id, collection, updatedImage) => {
+        const portraitureDoc = doc(
+            // db, "portraiture"
+            collection, 
+            id )
+
+        return updateDoc(portraitureDoc, updateDoc)
+    }
+
+    deleteImage = (id, collection, url) => {
+        const portraitureDoc = doc(
+            // db, "portraiture"
+            collection, 
+            id )
+
+            let pictureRef = ref(storage,url)
+            deleteObject(pictureRef)
+            .then(() => {
+                console.log("File deleted");
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+        return deleteDoc(portraitureDoc)
+    }
+
+    getAllImages = (collection, snapshot) => {
+        
+        return onSnapshot(collection, snapshot)
+
+        // return getDocs(collection)
+    }
+
+    getImage = (id, collection) => {
+        const portraitureDoc = doc(
+            // db, "portraiture"
+            collection, 
+            id )
+        return getDoc(portraitureDoc)
+    }
+}
+
+export default new ImageService()
