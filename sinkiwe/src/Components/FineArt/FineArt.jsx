b7c4bfa4820fef604801ea9e17ddba35c51e46db
danielthecode <35077377+danielthecode@users.noreply.github.com>
2022-04-26 06:58:04 +0100

    Updated design and animations for fineart component

--- sinkiwe/src/Components/FineArt/FineArt.jsx
+++ sinkiwe/src/Components/FineArt/FineArt.jsx
@@ -6,6 +6,8 @@ import { SRLWrapper } from "simple-react-lightbox";
 import ImageService from "../../utils/ImageService" 
 import { collection, query, orderBy } from "firebase/firestore"; 
 import {  db } from '../../firebase'
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const options = {
   thumbnails: {
@@ -21,9 +23,15 @@ function FineArt() {
         getPortraiture();
     }, [])
 
+
+    useEffect(() => {
+        AOS.init();
+        AOS.refresh();
+      }, []);
+
     const getPortraiture = async() => {
         
-        const data = await ImageService.getAllImages(query(collection(db, "fineArt"),orderBy("timeStamp", "desc")), (snapshot) => {
+        await ImageService.getAllImages(query(collection(db, "fineArt"),orderBy("timeStamp", "desc")), (snapshot) => {
             let imagesList = []
             snapshot.docs.forEach((doc) => {
                 imagesList.push({...doc.data(), id: doc.id})
@@ -41,12 +49,12 @@ function FineArt() {
 <MobileView>
         <div className="portraiture">
             <div className="container">
-                <div className="heading"><div className="text">FineArt</div></div>
+                <div className="heading"><div className="text" data-aos="fade-down" data-aos-easing="linear">FineArt</div></div>
                 <div className="scroll">scroll →</div>
                 <div className="image-wrapper">
                 {images.map((doc, id) => {
                     return (
-                      <div className="image-container" key={doc.id}>
+                      <div className="image-container" data-aos="fade-left" data-aos-easing="linear" data-aos-duration="1000" key={doc.id}>
                         <img src={doc.imageURL} className="img" alt="" />
                     </div>  
                     )
@@ -59,15 +67,15 @@ function FineArt() {
     <BrowserView>
 <div className="portraiture">
     <div className="container">
-        <div className="heading"><div className="text">Fine Art</div></div>
-        <div className="scroll">scroll →</div>
+        <div className="heading"><div className="text"><h4 data-aos="fade-left" data-aos-easing="linear" data-aos-duration="1000" data-aos-delay="500">Fine Art</h4></div></div>
+        <div className="scroll" data-aos="fade-left" data-aos-easing="linear" data-aos-duration="1000" data-aos-delay="500">scroll →</div>
         <div className="image-wrapper">
 
              <HorizontalScroll config={{ stiffness: 137, damping: 14 }} pageLock={true} reverseScroll={true} style={{height: "100%", width: "100%"}}>
             
             {images.map((doc, id) => {
                     return ( 
-                    <a href={doc.imageURL} className="image-container" key={doc.id}>
+                    <a href={doc.imageURL} className="image-container" data-aos="fade-down" data-aos-easing="linear" data-aos-duration="1100" data-aos-delay="1100" key={doc.id}>
                     <img src={doc.imageURL} className="img" alt="" />
                     </a>
                     )
