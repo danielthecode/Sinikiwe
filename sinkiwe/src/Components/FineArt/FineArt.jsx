3fe1de12635d2cb842269023b39d68f837de9254
danielthecode <35077377+danielthecode@users.noreply.github.com>
2022-04-26 06:54:23 +0100

    Updated design and animations for fineart component

--- sinkiwe/src/Components/FineArt/FineArt.jsx
+++ sinkiwe/src/Components/FineArt/FineArt.jsx
@@ -1,76 +1,77 @@
-import React from 'react'
-import image2 from "../../Images/fine1.jpg"
-import image3 from "../../Images/fine2.jpg"
-import image4 from "../../Images/fine3.jpg"
-import image5 from "../../Images/fine4.jpg"
-import image6 from "../../Images/fine5.jpg"
+/* eslint-disable no-undef */
+import React, {useState, useEffect} from 'react'
 import HorizontalScroll from 'react-scroll-horizontal'
 import { BrowserView, MobileView } from 'react-device-detect';
 import { SRLWrapper } from "simple-react-lightbox";
-import "./fineArt.scss"
+import ImageService from "../../utils/ImageService" 
+import { collection, query, orderBy } from "firebase/firestore"; 
+import {  db } from '../../firebase'
 
-
+const options = {
+  thumbnails: {
+      showThumbnails: true
+  }
+}
 
 function FineArt() {
-  return (
-    <>
-    <SRLWrapper>
 
+  const [images, setImages] = useState([])
+
+    useEffect(() => {
+        getPortraiture();
+    }, [])
+
+    const getPortraiture = async() => {
+        
+        const data = await ImageService.getAllImages(query(collection(db, "fineArt"),orderBy("timeStamp", "desc")), (snapshot) => {
+            let imagesList = []
+            snapshot.docs.forEach((doc) => {
+                imagesList.push({...doc.data(), id: doc.id})
+                setImages(imagesList)
+            })
+            console.log(imagesList);
+        })
+        
+    }
+
+
+  return (
+<>
+<SRLWrapper options={options}>
 <MobileView>
-        <div className="fineArt">
+        <div className="portraiture">
             <div className="container">
-                <div className="heading"><div className="text">Fine Art</div></div>
+                <div className="heading"><div className="text">FineArt</div></div>
+                <div className="scroll">scroll →</div>
                 <div className="image-wrapper">
-                    <div className="image-container">
-                        <img src={image2} className="img" alt="" />
-                    </div>
-                    <div className="image-container">
-                        <img src={image3} className="img" alt="" />
-                    </div>
-                    <div className="image-container">
-                        <img src={image4} className="img" alt="" />
-                    </div>
-                    <div className="image-container">
-                        <img src={image5} className="img" alt="" />
-                    </div>
-                    <div className="image-container">
-                        <img src={image6} className="img" alt="" />
-                    </div>
+                {images.map((doc, id) => {
+                    return (
+                      <div className="image-container" key={doc.id}>
+                        <img src={doc.imageURL} className="img" alt="" />
+                    </div>  
+                    )
+                    })}
                 </div>
           </div>
       </div>
     </MobileView>
 
     <BrowserView>
-    
-    <div className="fineArt">
+<div className="portraiture">
     <div className="container">
         <div className="heading"><div className="text">Fine Art</div></div>
         <div className="scroll">scroll →</div>
         <div className="image-wrapper">
 
              <HorizontalScroll config={{ stiffness: 137, damping: 14 }} pageLock={true} reverseScroll={true} style={{height: "100%", width: "100%"}}>
-            <a href={image2} className="image-container">
-                <img src={image2} className="img" alt="" />
-            </a>
-            <a href={image3} className="image-container">
-                <img src={image3} className="img" alt="" />
-            </a>
-            <a href={image4} className="image-container">
-                <img src={image4} className="img" alt="" />
-            </a>
-            <a href={image5} className="image-container">
-                <img src={image5} className="img" alt="" />
-            </a>
-            <a href={image6} className="image-container">
-                <img src={image6} className="img" alt="" />
-            </a>
-            <a href={image6} className="image-container">
-                <img src={image6} className="img" alt="" />
-            </a>
-            <a href={image6} className="image-container">
-                <img src={image6} className="img" alt="" />
-            </a>
+            
+            {images.map((doc, id) => {
+                    return ( 
+                    <a href={doc.imageURL} className="image-container" key={doc.id}>
+                    <img src={doc.imageURL} className="img" alt="" />
+                    </a>
+                    )
+            })}
         </HorizontalScroll>
        
 
@@ -80,8 +81,8 @@ function FineArt() {
 </div>
 
     </BrowserView>
-
     </SRLWrapper>
+    
     
     </>
   )
