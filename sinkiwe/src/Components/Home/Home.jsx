527d14c0a36cca7809985bb87c10912218ff94d3
danielthecode <35077377+danielthecode@users.noreply.github.com>
2022-04-26 06:42:03 +0100

    Added animations to home component

--- sinkiwe/src/Components/Home/Home.jsx
+++ sinkiwe/src/Components/Home/Home.jsx
@@ -1,16 +1,23 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import "./home.scss";
 import Image from "../../Images/Hero_image.png";
-
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 function Home() {
+
+  useEffect(() => {
+    AOS.init();
+    AOS.refresh();
+  }, []);
+
   return (
     <div className="home">
       <div className='container'>
             <div className='hero-image'><img src={Image} className="img" alt="" /></div>
             <div className="heading">
-              SINIKIWE
-              <div className="sub-heading">photographer</div>
+              <h4 data-aos="fade-down" data-aos-easing="linear" data-aos-duration="1000" data-aos-delay="100">SINIKIWE</h4>
+              <div className="sub-heading"><h5 data-aos="fade-down" data-aos-easing="linear" data-aos-duration="1000" data-aos-delay="100">hotographer</h5></div>
             </div>
             
     </div>
