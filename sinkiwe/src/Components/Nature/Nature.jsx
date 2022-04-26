e545ecda693f4ac9a5a5a1b49d137012448e3a6c
danielthecode <35077377+danielthecode@users.noreply.github.com>
2022-04-26 06:53:49 +0100

    Updated design and animations for nature component

--- sinkiwe/src/Components/Nature/Nature.jsx
+++ sinkiwe/src/Components/Nature/Nature.jsx
@@ -1,15 +1,11 @@
-import React from 'react'
-import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-import image2 from "../../Images/nature1.jpg"
-import image3 from "../../Images/nature2.jpg"
-import image4 from "../../Images/nature3.jpg"
-import image5 from "../../Images/nature4.jpg"
-import image1 from "../../Images/IMG_2330-min.jpg"
+/* eslint-disable no-unused-vars */
+import React, {useState, useEffect} from 'react'
 import "./nature.scss"
 import HorizontalScroll from 'react-scroll-horizontal'
 import { BrowserView, MobileView } from 'react-device-detect';
-import { SRLWrapper } from "simple-react-lightbox";
+import ImageService from "../../utils/ImageService" 
+import { collection, query, orderBy } from "firebase/firestore"; 
+import {  db } from '../../firebase'
 // import styles
 import 'lightgallery/css/lightgallery.css';
 import 'lightgallery/css/lg-zoom.css';
@@ -18,11 +14,33 @@ import LightGallery from 'lightgallery/react';
 import lgZoom from 'lightgallery/plugins/zoom';
 import lgVideo from 'lightgallery/plugins/video';
 import lgThumbnail from "lightgallery/plugins/thumbnail";
-
-
-gsap.registerPlugin(ScrollTrigger);
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 function Nature() {
+
+    const [images, setImages] = useState([])
+
+    useEffect(() => {
+        getPortraiture();
+    }, [])
+
+    useEffect(() => {
+        AOS.init();
+        AOS.refresh();
+      }, []);
+
+    const getPortraiture = async() => {
+        
+        const data = await ImageService.getAllImages(query(collection(db, "nature"),orderBy("timeStamp", "desc")), (snapshot) => {
+            let imagesList = []
+            snapshot.docs.forEach((doc) => {
+                imagesList.push({...doc.data(), id: doc.id})
+                setImages(imagesList)
+            })
+        })
+        
+    }
 
   return (
     <>
@@ -30,22 +48,17 @@ function Nature() {
 <MobileView>
         <div className="nature">
             <div className="container">
-                <div className="heading"><div className="text">Nature</div></div>
-                <div className="scroll">scroll →</div>
+                <div className="heading"><div className="text" data-aos="fade-down" data-aos-easing="linear">Nature</div></div>
+                <div className="scroll" data-aos="fade-down" data-aos-easing="linear" data-aos-duration="1000" data-aos-delay="500">scroll →</div>
                 <div className="image-wrapper">
                 <LightGallery plugins={[lgZoom, lgVideo, lgThumbnail]} mode="lg-fade">
-                    <a href={image2} className="image-container">
-                        <img src={image2} className="img" alt="" />
-                    </a>
-                    <a href={image3} className="image-container">
-                        <img src={image3} className="img" alt="" />
-                    </a>
-                    <a href={image4} className="image-container">
-                        <img src={image4} className="img" alt="" />
-                    </a>
-                    <a href={image5} className="image-container">
-                        <img src={image5} className="img" alt="" />
-                    </a>
+                {images.map((doc, id) => {
+                    return (
+                      <a href={doc.imageURL} data-aos="fade-down" data-aos-easing="linear" data-aos-duration="1000" className="image-container" key={doc.id}>
+                        <img src={doc.imageURL} className="img" alt="" />
+                    </a>  
+                    )
+                    })}
                     </LightGallery>
                 </div>
           </div>
@@ -55,24 +68,19 @@ function Nature() {
     <BrowserView>
     <div className="nature">
     <div className="container">
-        <div className="heading"><div className="text">Nature</div></div>
-        <div className="scroll">scroll →</div>
+        <div className="heading"><div className="text"><h4 data-aos="fade-down" data-aos-easing="linear" data-aos-duration="1000" data-aos-delay="500">Nature</h4></div></div>
+        <div className="scroll" data-aos="fade-down" data-aos-easing="linear" data-aos-duration="1000" data-aos-delay="500">scroll →</div>
         <div className="image-wrapper">
 
              <HorizontalScroll config={{ stiffness: 137, damping: 14 }} pageLock={true} reverseScroll={true} style={{height: "100%", width: "100%"}}>
              <LightGallery plugins={[lgZoom, lgVideo, lgThumbnail]} mode="lg-fade">
-                    <a href={image2} className="image-container">
-                        <img src={image2} className="img" alt="" />
-                    </a>
-                    <a href={image3} className="image-container">
-                        <img src={image3} className="img" alt="" />
-                    </a>
-                    <a href={image4} className="image-container">
-                        <img src={image4} className="img" alt="" />
-                    </a>
-                    <a href={image5} className="image-container">
-                        <img src={image5} className="img" alt="" />
-                    </a>
+             {images.map((doc, id) => {
+                    return (
+                      <a href={doc.imageURL} className="image-container" data-aos="fade-down" data-aos-easing="linear" data-aos-duration="1200" data-aos-delay="500" key={doc.id}>
+                        <img src={doc.imageURL} className="img" alt="" />
+                    </a>  
+                    )
+                    })}
             </LightGallery>
         </HorizontalScroll>
        
