ea9a41f37788c11ae2d6a3931ec185a9c67f46da
danielthecode <35077377+danielthecode@users.noreply.github.com>
2022-04-26 07:09:45 +0100

    Updated Index.js and App.js for routing plus miscellaneous content

--- sinkiwe/src/index.js
+++ sinkiwe/src/index.js
@@ -1,39 +1,15 @@
 import React from 'react';
-import {createRoot} from 'react-dom/client';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import Header from './Components/Header/Header';
-import Potraiture from './Components/Portraiture/Potraiture';
-import Nature from './Components/Nature/Nature';
-import Weddings from './Components/Weddings/Weddings';
-import FineArt from './Components/FineArt/FineArt';
-import Home from './Components/Home/Home';
-import Test from './Components/Test/Test.tsx'
-import "./index.scss"
-import SimpleReactLightbox from 'simple-react-lightbox'
-import ScrollToTop from "./Components/ScrollToTop.jsx";
-import UploadPortraiture from "./Components/Admin/Portraiture/Upload/UploadPortraiture.jsx"
+import { createRoot } from 'react-dom/client';
+import App from './App'
+
+import {AuthContextProvider} from './contexts/AuthContext'
 
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
 root.render(
-  <SimpleReactLightbox>
-  <Router>
-    <ScrollToTop>
-    <Header/>
-    <Routes>
-
-      <Route path="/" element={<Home/>}/>
-      <Route path="/home" element={<Home/>}/>
-      <Route path="/portraiture" element={<Potraiture/>}/>
-      <Route path="/nature" element={<Nature/>}/>
-      <Route path="/weddings" element={<Weddings/>}/>
-      <Route path="/fineart" element={<FineArt/>}/>
-      <Route path="/admin/portraiture-upload" element={<UploadPortraiture/>}/>
-    </Routes>
-    </ScrollToTop>
-    {/* <Footer/> */}
-  </Router>
-  </SimpleReactLightbox>
+        <AuthContextProvider>
+        <App/>
+        </AuthContextProvider>
 );
